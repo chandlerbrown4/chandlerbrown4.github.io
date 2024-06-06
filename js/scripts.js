e020b01b93db1907bdc6ff03502fe686906c385d
brownce <evan.brown.us@gmail.com>
2024-06-06 19:11:37 -0400

    speed animation change

--- js/scripts.js
+++ js/scripts.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             move: {
                 enable: true,
-                speed: 6,
+                speed: 3,
                 direction: 'none',
                 random: false,
                 straight: false,
